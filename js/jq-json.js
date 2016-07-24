6343ec090a6bbcc43068a3e362896f527a152037
Brian0610 <brian49345@gmail.com>
2016-07-23 21:17:42 -0400

    updated javascript

--- js/jq-json.js
+++ js/jq-json.js
@@ -4,9 +4,15 @@ $(function() {
 
 	function loadRates() {
 		$.getJSON('data/rates.json')
-		.done( function(){
-			alert('success'); 
-
+		.done( function(data){
+			var d = new Date(); 
+			var h = d.getHours();
+			var m = d.getMinutes();  
+			var msg = '<h2>Exchange Rates</h2>'; 
+			$.each(data, function(key, val){
+				msg += '<div class="' + key + '">$' + key + ': ' + val + '</div>'
+			}); 
+			$('#rates').append(msg); 
 		}).fail( function(){
 			alert('fail'); 
 			// code goes here
